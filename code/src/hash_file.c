4d8ba4b272c9c7807ebbeb05156f08100d9d6132
DimitrisRammos <dimitris.p.rammos@gmail.com>
2021-11-30 17:32:07 +0200

    Add makefile,init

--- code/src/hash_file.c
+++ code/src/hash_file.c
@@ -15,13 +15,48 @@
   }                         \
 }
 
-HT_ErrorCode HT_Init() {
+
+typedef struct HT {
+	char** table_file;
+} HT_tables;
+
+HT_tables* tables;
+
+/*
+ * Η συνάρτηση HT_Init χρησιμοποιείται για την αρχικοποίηση κάποιον δομών που μπορεί να χρειαστείτε. 
+ * Σε περίπτωση που εκτελεστεί επιτυχώς, επιστρέφεται HT_OK, ενώ σε διαφορετική περίπτωση κωδικός λάθους.
+ */
+HT_ErrorCode HT_Init() 
+{
   //insert code here
+  tables = malloc(sizeof(struct HT));
+  tables->table_file = malloc(20*sizeof(char*));
+  if(tables->table_file == NULL)
+  {
+    return HT_ERROR;
+  }
+  for(int i = 0; i < 20; i++)
+  {
+    tables->table_file[i] = malloc(100*sizeof(char));
+    if( tables->table_file[i] == NULL)
+    {
+      return HT_ERROR;
+    }
+  }
+
+  tables->table_file[0] = "kapa";
   return HT_OK;
 }
 
+
+/*
+ * Η συνάρτηση HT_CreateIndex χρησιμοποιείται για τη δημιουργία και κατάλληλη αρχικοποίηση ενός άδειου αρχείου κατακερματισμού με όνομα fileName. 
+ * Στην περίπτωση που το αρχείο υπάρχει ήδη, τότε επιστρέφεται ένας κωδικός λάθους. 
+ * Σε περίπτωση που εκτελεστεί επιτυχώς επιστρέφεται HΤ_OK, ενώ σε διαφορετική περίπτωση κωδικός λάθους.
+ */
 HT_ErrorCode HT_CreateIndex(const char *filename, int depth) {
   //insert code here
+  printf("hello:  %s\n",tables->table_file[0]);
   return HT_OK;
 }
 
