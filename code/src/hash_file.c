c3b0d58e428515fd4f6e9584e8c70eaa1b3f1605
DimitrisRammos <dimitris.p.rammos@gmail.com>
2021-12-05 13:47:55 +0200

    create

--- code/src/hash_file.c
+++ code/src/hash_file.c
@@ -73,10 +73,20 @@ struct FileInMem{
 FileTable filetable;
 
 
+//αποθηκευουμε τον αριθμο θεσης πινακα 0,1....,2^depth
+//για καθε θεση σε πoιο block δειχνει
+struct position{
+  int pos;
+  int num_block;
+};
+
+typedef struct position* Position;
 
 
 
-/*
+
+
+/* 
  * Η συνάρτηση HT_Init χρησιμοποιείται για την αρχικοποίηση κάποιον δομών που μπορεί να χρειαστείτε. 
  * Σε περίπτωση που εκτελεστεί επιτυχώς, επιστρέφεται HT_OK, ενώ σε διαφορετική περίπτωση κωδικός λάθους.
  */
@@ -102,10 +112,7 @@ HT_ErrorCode HT_Init()
  */
 HT_ErrorCode HT_CreateIndex(const char *filename, int depth) {
   //insert code here
-  
-  
-  //μια μεταβλητη bool αν υπαρχει ή οχι
-  
+    
   int file_desc;
 
   // CALL_BF(BF_Init(LRU));
@@ -125,53 +132,91 @@ HT_ErrorCode HT_CreateIndex(const char *filename, int depth) {
     num_of_blocks++;
   }
 
-  // BF_Block*** table;
-  // table = malloc(depth*sizeof(BF_Block***));
-  // for( int i = 0; i < depth; i++)
-  // {
-  //   table[i] = malloc(sizeof(BF_Block**));
-    
-  // }
-  // printf("Bl size: %d\n", sizeof(table));
+  //φτιαχνω ενα μπλοκ και βαζω μεσα το βαθος
   BF_Block *block;
   BF_Block_Init(&block);
 
   CALL_BF( BF_AllocateBlock( file_desc, block));
-  int* data;
+  char* data;
 
   data = BF_Block_GetData(block);
   memcpy(data, &depth, sizeof(int));
   BF_Block_SetDirty(block);
-  data = BF_Block_GetData(block);
+ 
+  //Πρεπει να το ξαναδω αν σε ενοχλεί βάλτο σε σχόλια μην το σβήσεις.
+  int i = 0;
+  int num_of_ints = 0;
+  data = BF_Block_GetData(block) + sizeof(int); //pairno to data
 
-  printf("edwsa data %d\n", *data);
+  //FOR HASH-TABLE
+  while (num_of_ints < a)        //ελεγχουμε αν χωραει στο ιδιο μπλοκ το αρχικο μας hash-table
+  {
+    if(data+i*sizeof(struct position) > data+BF_BLOCK_SIZE-1)
+    {
+      CALL_BF( BF_AllocateBlock( file_desc, block));
+      data = BF_Block_GetData( block);
+      i = 0;
+    }
+    Position pos = malloc(sizeof(struct position));
+    
+    data = data + i*sizeof(struct position);
+    memcpy( data + i*sizeof(struct position), pos, sizeof(struct position));
+    BF_Block_SetDirty(block);
 
-  CALL_BF(BF_UnpinBlock(block));
-
-  // CALL_BF( BF_AllocateBlock( file_desc, block));
-
-  // //Πρεπει να το ξαναδω αν σε ενοχλεί βάλτο σε σχόλια μην το σβήσεις.
-  // int i = 0;
-  // int num_of_ints = 0;
-  // while (num_of_ints < a){
-  //   if(block+i*sizeof(int) > block+BF_BLOCK_SIZE-1){
-  //     CALL_BF( BF_AllocateBlock( file_desc, block));
-  //     i = 0;
-  //   }
-  //   memcpy(block+i*sizeof(int), num_of_ints, sizeof(int));
-  //   num_of_ints++;
-  //   i++;
-  // }
-  
-  //se ena block Plirofories
-  //bathos - filename
-
-
-  //se ena deytero to pointer tou table
-
-  //alla block gia egrafes gia arxi 4
+    num_of_ints++;
+    i++;
+  }
 
 
+  //print all elements
+  data = BF_Block_GetData( block);
+  for( int i = 0; i < 5; i++)
+  {
+    if( i == 0)
+    {
+      printf("EDO EIMAI   %d\n",data[0]);
+    }
+    else
+    {
+      // Position p;
+      if( i == 1)
+      {
+        data = data + i*sizeof(int);
+      }
+      else
+      {
+        data = data +i*sizeof(struct position);
+      }
+
+      Position p;
+      p = data;
+      int n1 = p->pos;
+      int n2 = p->num_block;
+
+      printf("EDO EIMAI   %d -- %d size:%d\n", n1, n2, sizeof(struct position));
+    }
+
+
+  }
+
+  //FOR BUCKET IN HASHTABLE 
+  //1BUCKET = 1BLOCK
+  for( int i = 0; i < a; i++)
+  {
+    CALL_BF(BF_AllocateBlock(file_desc, block));
+  }
+
+  //Unpin the blocks
+  int blocks_num;
+  CALL_BF( BF_GetBlockCounter( file_desc, &blocks_num));
+  printf("eco blocks: %d\n", blocks_num);
+  for( int i = 0; i < blocks_num; i++)
+  {
+    CALL_BF( BF_GetBlock(file_desc, i, block));
+    CALL_BF( BF_UnpinBlock( block));
+  }
+
+  BF_Block_Destroy( &block);
   CALL_BF( BF_CloseFile(file_desc));
 
 
